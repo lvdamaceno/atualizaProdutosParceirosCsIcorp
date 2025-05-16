1f99b87d56102f25dab48e5cb7021ce99fc5f6a0
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2025-05-16 16:47:58 -0300

    Refactor

--- update_geral/cs_sender.py
+++ update_geral/cs_sender.py
@@ -1,10 +1,16 @@
+import json
 import logging
 import os
 import time
 
 import requests
+from dotenv import load_dotenv
 
-from update_geral.utils import util_cs_enpoint
+from update_geral.sankhya_fetch import snk_fetch_data, snk_fetch_json
+from update_geral.utils import util_cs_enpoint, util_remove_brackets, tempo_restante, logging_config
+
+load_dotenv()
+logging_config()
 
 def cs_enviar(dados: list[dict], tipo) -> dict:
     endpoint = util_cs_enpoint(tipo)
@@ -32,3 +38,72 @@ def cs_enviar(dados: list[dict], tipo) -> dict:
         time.sleep(tentativa * 2)  # espera incremental: 2s, 4s, 6s...
 
     return {"erro": f"Falha após {tentativas} tentativas ao enviar {tipo} para CS."}
+
+
+def cs_processar_envio_parceiro(sql, tamanho_lote: int = 100):
+    logging.info("=" * 60)
+    logging.info(f"👤 INICIANDO PROCESSAMENTO DE PARCEIRO...")
+    logging.info("=" * 60)
+    codparcs = snk_fetch_data(sql)
+    total = len(codparcs)
+    inicio_total = time.time()
+
+    for i in range(0, total, tamanho_lote):
+        lote = codparcs[i:i + tamanho_lote]
+        json_lote = []
+        inicio_lote = time.time()
+
+        for row in lote:
+            codparc = row[0]
+            json_raw = snk_fetch_json(codparc, "parceiro")
+            json_limpo = util_remove_brackets(json_raw)
+            try:
+                json_dict = json.loads(json_limpo)
+                json_lote.append(json_dict)
+            except Exception as e:
+                logging.warning(f"❌ Erro ao decodificar JSON do parceiro {codparc}: {e}")
+                continue
+
+        logging.info(f"📦 Enviando lote {i // tamanho_lote + 1} com {len(json_lote)} registros...")
+        resposta = cs_enviar(json_lote, "parceiro")
+
+        tempo_restante(i, lote, inicio_lote, total, resposta)
+
+    duracao_total = time.time() - inicio_total
+    logging.info(f"🏁 Processamento completo em {duracao_total:.1f} segundos.")
+
+
+def cs_processar_envio(tipo, sql, tamanho_lote: int = 100):
+    logging.info("=" * 60)
+    logging.info(f"🟢 INICIANDO PROCESSAMENTO DE {tipo}...")
+    logging.info("=" * 60)
+    codprods = snk_fetch_data(sql)
+    total = len(codprods)
+    inicio_total = time.time()
+
+    for i in range(0, total, tamanho_lote):
+        lote = codprods[i:i + tamanho_lote]
+        json_lote = []
+        inicio_lote = time.time()
+
+        for row in lote:
+            codprod = row[0]
+            json_raw = snk_fetch_json(codprod, tipo)
+            try:
+                json_corrigido = f"[{json_raw}]".replace("}{", "},{")
+                lista = json.loads(json_corrigido)
+                json_lote.extend(lista)
+            except Exception as e:
+                logging.warning(f"❌ Erro ao decodificar JSON do {tipo} {codprod}: {e}")
+                continue
+
+        # Json completo
+        # logging.debug(json.dumps(json_lote, indent=2, ensure_ascii=False))
+
+        logging.info(f"📦 Enviando lote {i // tamanho_lote + 1} com {len(json_lote)} registros...")
+        resposta = cs_enviar(json_lote, tipo)
+
+        tempo_restante(i, lote, inicio_lote, total, resposta)
+
+    duracao_total = time.time() - inicio_total
+    logging.info(f"🏁 Processamento completo de {tipo} em {duracao_total:.1f} segundos.")
---
+++ update_geral/main.py
@@ -0,0 +1,16 @@
+from update_geral.cs_sender import cs_processar_envio, cs_processar_envio_parceiro
+
+if __name__ == "__main__":
+    lista_parceiros_para_enviar = "SELECT TOP 5 CODPARC FROM TGFPAR WHERE CGC_CPF IN (SELECT CPF FROM TFPFUN)"
+    cs_processar_envio_parceiro(lista_parceiros_para_enviar, tamanho_lote=50)
+
+
+    lista_produtos_para_enviar = """
+                                  SELECT TOP 1 CODPROD 
+                                  FROM TGFPRO 
+                                  WHERE CODGRUPOPROD <= '1159999' 
+                                    AND USOPROD = 'R' 
+                                    AND ATIVO = 'S'
+                                  """
+    cs_processar_envio("produto", lista_produtos_para_enviar, tamanho_lote=50)
+    cs_processar_envio("estoque", lista_produtos_para_enviar, tamanho_lote=50)
