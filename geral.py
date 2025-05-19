3b120036004a8f832b99f23215525622c6eaaedd
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2025-05-19 00:43:42 -0300

    Notification

--- geral.py
+++ geral.py
@@ -1,10 +1,13 @@
 from processamentos import *
+from telegram_notification import enviar_notificacao_telegram
 
 
 def envio_geral(step, lote, workers):
     processar_produtos(step, lote, workers)
     processar_parceiros(step, lote, workers)
+    enviar_notificacao_telegram("🏁 Atualização geral Sankhya-Icorp finalizada")
 
 
 if __name__ == "__main__":
+    enviar_notificacao_telegram("🚀 Atualização geral Sankhya-Icorp iniciada")
     envio_geral(50, 10, 35)
